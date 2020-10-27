bf5916878f6485531b909308e41a5cf08d5345bd
Lisandro Saez <lisandrosaezamelio@gmail.com>
2020-10-27 00:07:57 -0300

    prueba1

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoalA: 'Finish the course and learn Vue',
+            courseGoalA: 'Finish the course and learn VueGit',
             courseGoalB: 'Master Vue and build amazing apps!',
             vueLink: 'https://vuejs.org/'
         };
